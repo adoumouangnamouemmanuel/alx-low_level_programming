775359c0b6c606c2b526ae43aefc9777a906c656
adoumouangnamouemmanuel <adoumouangnamouemmanuel@gmail.com>
2023-03-17 13:45:27 -0700

    Task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	/* a program that prints all single digit numbers of base 10 starting from 0, followed by a new line.
+	 *
+	 *
+	 * return always 0
+	 */
+	int a = 0;
+	for (a = 0; a<10; a++)
+		printf("%d\n", a)
+}
