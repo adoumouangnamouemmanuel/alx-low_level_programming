3f6e37b71441099906c77ea5c5aacd3b1ee0656c
adoumouangnamouemmanuel <adoumouangnamouemmanuel@gmail.com>
2023-03-21 05:20:00 -0700

    Header

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -16,9 +16,9 @@ int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
-void time_teble(void);
+void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-void print_time_table(int n);
+void print_times_table(int n);
 
 #endif
