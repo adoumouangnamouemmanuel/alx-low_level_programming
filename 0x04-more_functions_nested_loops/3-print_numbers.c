0ba7ccdf0b60b27e2de2246b5f0c870115ed8965
adoumouangnamouemmanuel <adoumouangnamouemmanuel@gmail.com>
2023-03-23 04:17:45 -0700

    Task3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
- * void print_numbers - Prints number from 0 to 9
+ * print_numbers - Prints number from 0 to 9
  *
  */
 void print_numbers(void)
 {
 	int i;
-	for (i = 0; i < 9; i++)
+
+	for (i = 0; i <= 9; i++)
 	{
 		_putchar('0' + i);
 	}
