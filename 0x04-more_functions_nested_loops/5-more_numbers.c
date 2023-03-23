47c56969e83c2e864cabf1585c659864dc87748c
adoumouangnamouemmanuel <adoumouangnamouemmanuel@gmail.com>
2023-03-23 02:41:55 -0700

    Task5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * void more_numbers - prints 10 times the numbers from 0 to 14
+ *
+ */
+void more_numbers(void)
+{
+	int i = 0;
+	while(i < 10)
+	{
+		int n = 0;
+		while(n <= 14)
+		{
+			if (n > 9)
+			{
+				_putchar('0' + (n / 10));
+			}
+			_putchar('0' + (n % 10));
+			n++;
+		}
+		i++;
+		_putchar('\n');
+	}
+}
