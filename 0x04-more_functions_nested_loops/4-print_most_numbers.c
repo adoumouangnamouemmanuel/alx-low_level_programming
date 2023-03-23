79689a2da7d7050e0312883bb63658b5d658af99
adoumouangnamouemmanuel <adoumouangnamouemmanuel@gmail.com>
2023-03-23 04:52:58 -0700

    Task4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,17 +1,27 @@
 #include "main.h"
+
 /**
- * void print_most_numbers - prints digit from 0 to 9 except 2 and 4;
+ * print_most_numbers - Prints 0-9 leaving out 2 and 4.
+ *
+ * Return: 0-9
  *
  */
+
 void print_most_numbers(void)
 {
-	int n;
-	for (n = 0; n < 9; n++)
+	int i = 0;
+
+	for (; i <= 9; i++)
 	{
-		if (n != 2 && n != 4)
+		if (i == 2 || i == 4)
 		{
-			_putchar('0' + n);
+			continue;
+		}
+		else
+		{
+			_putchar(i + '0');
 		}
 	}
+
 	_putchar('\n');
 }
