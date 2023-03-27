afadd58a000e255596d0dd4b0fe9f0a49aca36ec
adoumouangnamouemmanuel <adoumouangnamouemmanuel@gmail.com>
2023-03-27 04:31:38 -0700

    Task4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,31 +1,23 @@
 #include "main.h"
 
 /**
- * rev_string - prints a string in reverse
+ * print_rev - reverse string
+ *
  * @s: String to be reversed
  *
  * Return: void
  */
 
-void rev_string(char *s)
+
+void print_rev(char *s)
 {
-	int i = 0, j, right;
+	int i = 0;
 
 	while (s[i])
 		i++;
 
-	j = i / 2;
-	right = 0;
+	while (i--)
+		_putchar(s[i]);
 
-	while (right != j)
-	{
-		char temp = s[right];
-		int left = i - right - 1;
-
-		s[right] = s[left];
-		s[left] = temp;
-
-		right++;
-	}
-
+	_putchar('\n');
 }
