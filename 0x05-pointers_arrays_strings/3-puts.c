2fad65b55309c518a66794fbdccda7694cea157c
adoumouangnamouemmanuel <adoumouangnamouemmanuel@gmail.com>
2023-03-27 04:16:07 -0700

    Task3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line
+ *
+ * @str : The variable that store the stirng
+ *
+ * Return : Nothing
+ */
+
+void _puts(char *str)
+{
+	while (*s != '\0')
+	{
+		_putchar(s);
+		s++;
+	}
+}
