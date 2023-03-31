4f269bfe8a0ec2ab4ca4d07c4c53cbf492a1497f
adoumouangnamouemmanuel <adoumouangnamouemmanuel@gmail.com>
2023-03-31 14:29:54 -0700

    Task9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,25 +1,22 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
-  * print_number - Prints any integer with putchar
-  * @n: Number to prints
-  *
-  * Return: Nothing
-  */
+ * print_number - Prints an integer.
+ * @n: The integer to be printed.
+ */
+
 void print_number(int n)
 {
-	unsigned int x;
+	unsigned int num = n;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		n *= -1;
+		num = -num;
 	}
 
-	x = n;
+	if ((num / 10) > 0)
+		print_number(num / 10);
 
-	if (x / 10)
-		print_number(x / 10);
-
-	_putchar(x % 10 + '0');
+	_putchar((num % 10) + '0');
 }
