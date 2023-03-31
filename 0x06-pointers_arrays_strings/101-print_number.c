f2262a571298614ee97ebaf2198563ec2be6c0db
adoumouangnamouemmanuel <adoumouangnamouemmanuel@gmail.com>
2023-03-31 14:13:06 -0700

    Task11

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,32 +1,14 @@
 #include "main.h"
 
 /**
- * base10 - power in 10 base
- * @n: an exponent
- * Return: returns 10 to power exponent
- */
-int base10(int n)
-{
-	int base = 10;
-
-	while (n > 0)
-	{
-		base *= 10;
-		n--;
-	}
-	return (base);
-}
-
-/**
- * print_number - prints integers enters as parameters using putchar
- * @n: integer to print
- * Return: void
- */
+  * print_number - Prints any integer with putchar
+  * @n: Number to prints
+  *
+  * Return: Nothing
+  */
 void print_number(int n)
 {
-	int power;
-
-	power = base10(8);
+	unsigned int x;
 
 	if (n < 0)
 	{
@@ -34,19 +16,10 @@ void print_number(int n)
 		n *= -1;
 	}
 
-	if (n == 0)
-		_putchar('0');
+	x = n;
 
-	else
-	{
-		while (n / power == 0)
-			power /= 10;
+	if (x / 10)
+		print_number(x / 10);
 
-		while (power >= 1)
-		{
-			_putchar((n / power) + '0');
-			n %= power;
-			power /= 10;
-		}
-	}
+	_putchar(x % 10 + '0');
 }
