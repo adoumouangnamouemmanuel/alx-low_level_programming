2c55f99d66fb28cfeb3fac4415dc0345878e0548
adoumouangnamouemmanuel <adoumouangnamouemmanuel@gmail.com>
2023-04-05 16:47:47 -0700

    Task0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,11 +7,11 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s == '\0')
 	{
-		_putchar(s);
-		_puts_recursion(s + 1);
+		return;
 	}
-	else
-		_putchar('\n');
+	_putchar(*s);
+	_puts_recursion(s + 1);
+	_putchar('\n');
 }
