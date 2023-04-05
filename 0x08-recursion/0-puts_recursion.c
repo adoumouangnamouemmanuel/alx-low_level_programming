13d342bf42ca31e7906601c357fc92d0adab7ddb
adoumouangnamouemmanuel <adoumouangnamouemmanuel@gmail.com>
2023-04-05 05:18:10 -0700

    recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string, followed by a new line.
+ * @s : a variable that points the value
+ * Return : Nothing
+ */
+void _puts_recursion(char *s)
+{
+	int n = 0;
+
+	while (s != '\0')
+	{
+		_putchar(s[n]);
+		n++;
+	}
+	_putchar('\n');
+}
