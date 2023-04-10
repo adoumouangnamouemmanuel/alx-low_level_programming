ea7b998a2bc61e55942b4f7afe2f7ecf41aa50a8
adoumouangnamouemmanuel <adoumouangnamouemmanuel@gmail.com>
2023-04-10 13:13:17 -0700

    C file, source code

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string
+ *
+ * @s: The character containing the string
+ *
+ * Return: The length of the character
+ */
+
+int _strlen(char *s)
+{
+	int b = 0;
+
+	while (*s != '\0')
+	{
+		b++;
+		s++;
+	}
+	return (b);
+}
