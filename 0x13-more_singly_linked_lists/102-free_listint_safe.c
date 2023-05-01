7c190b1ba6674172fec74d685822a419b0aca7ce
adoumouangnamouemmanuel <adoumouangnamouemmanuel@gmail.com>
2023-05-01 15:46:09 -0700

    project 0x13 C

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,5 +1,5 @@
 #include "lists.h"
-#include <stdio.h>
+
 /**
  * free_listint_safe - frees a linked list
  * @h: pointer to the first node in the linked list
@@ -21,11 +21,13 @@ size_t free_listint_safe(listint_t **h)
 		if (diff > 0)
 		{
 			temp = (*h)->next;
+			free(*h);
 			*h = temp;
 			len++;
 		}
 		else
 		{
+			free(*h);
 			*h = NULL;
 			len++;
 			break;
