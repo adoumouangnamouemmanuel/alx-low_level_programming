088d6b42d1d890ac18c9e628732071305e8b8e65
adoumouangnamouemmanuel <adoumouangnamouemmanuel@gmail.com>
2023-06-07 07:38:32 -0700

    Recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _pow_recursion - return the power of number
+ * @x: number;
+ * @y: power
+ * Return: power
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x, y-1);
+}
