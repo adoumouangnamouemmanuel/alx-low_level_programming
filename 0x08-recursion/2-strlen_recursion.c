8dc2850e58ff0bda1ff56b06a0e5b68f4fd0ca4d
adoumouangnamouemmanuel <adoumouangnamouemmanuel@gmail.com>
2023-06-07 06:55:07 -0700

    recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,11 +6,8 @@
  */
 int _strlen_recursion(char *s)
 {
-	int len = 0;
-	if (*s != '\0')
-	{
-		len++;
-		_strlen_recursion(s + 1);
-	}
-	return (len);
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
 }
