59b8065935af05f3c5ed3dbbae652d4218855edb
adoumouangnamouemmanuel <adoumouangnamouemmanuel@gmail.com>
2023-06-09 08:02:30 -0700

    0x0A. C - argc, argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,20 +11,19 @@
  */
 int main(int argc, char *argv[])
 {
+	int num1, num2, prod;
+
 	if (argc != 3)
 	{
-		printf("Error");
+		printf("Error\n");
 		return (1);
 	}
-	else
-	{
-		int mul;
-		int num1 = atoi(argv[1]);
-		int num2 = atoi(argv[2]);
 
-		mul = num1 * num2;
-		printf("%d\n", mul);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	prod = num1 * num2;
 
-		return (0);
-	}
+	printf("%d\n", prod);
+
+	return (0);
 }
