807c692bf39de96c715cf2c77d1d151569a9a607
adoumouangnamouemmanuel <adoumouangnamouemmanuel@gmail.com>
2023-06-12 13:00:57 +0100

    malloc and free

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -15,7 +15,7 @@ char *_strdup(char *str)
 		return (NULL);
 	for (i = 0; str[i] != '\0'; i++)
 		;
-	strout = (char *)malloc(sizeof(char) * (i + 1));	
+	strout = (char *)malloc(sizeof(char) * (i + 1));
 	if (strout == NULL)
 		return  (NULL);
 	for (j = 0; j <= i; j++)
