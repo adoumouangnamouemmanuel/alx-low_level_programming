908a3117edc5c8c9801d744dd26ceff6f3ae0ce2
adoumouangnamouemmanuel <adoumouangnamouemmanuel@gmail.com>
2023-06-15 02:40:37 -0700

    more malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * malloc_checked -  allocates memory using malloc.
+ * @b: size
+ * Return void
+ */
+void *malloc_checked(unsigned int b)
+{
+	char *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
