dda6639852410d212f9dbf0bda641fcca4249166
adoumouangnamouemmanuel <adoumouangnamouemmanuel@gmail.com>
2023-08-22 08:54:22 -0700

    Dynamic library

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gcc -wall -wextra -werror -pedantic -c -fPIC *.c
+gcc -Wall -Wextra -Werror -pedantic -c -fPIC *.c
 gcc -o -shared liball.so *.o
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
